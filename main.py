9d6016cd7337f19a3098382a407dca96f46082ec
Gaby Rodriguez <leapinlab@gmail.com>
2025-02-07 12:15:59 -0700

    Commented everyone's sections

---
+++ main.py
@@ -0,0 +1,20 @@
+# Simulator class - James
+#   list of SML instructions
+
+#   define registers
+
+#   define memory and index
+
+# RunSimulator() function - Jonah
+
+# Print user instructions - Gaby
+
+# Load user instructions - Gaby
+
+# Validate() function - Jonah
+
+# TestOverflow() function - Jonah
+
+# Execute() function - Gaby
+
+# Unit Tests - Cortland
