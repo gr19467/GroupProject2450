95a53df51f01d8451a586bfa2d938d35b813fa55
Gaby Rodriguez <leapinlab@gmail.com>
2025-02-08 13:10:09 -0700

    Finished print_instructions

--- main.py
+++ main.py
@@ -8,8 +8,16 @@
 # RunSimulator() function - Jonah
 
 # Print user instructions - Gaby
+def print_instructions():
+    print("Welcome to UVSim! \n"
+          "Please enter your program one instruction \n"
+          "or data word at a time into the input text field. \n"
+          "I will display the location number and a question mark, \n"
+          "then you type in the word for that location. To stop \n"
+          "input, enter -99999.")
 
 # Load user instructions - Gaby
+
 
 # Validate() function - Jonah
 
