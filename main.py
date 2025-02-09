9c7c4b1d2f0ca9606668357c1485e1e8a177a840
James Durfee <jamesddurfee@gmail.com>
2025-02-08 20:16:41 -0700

    added simulator class

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 # Simulator class - James
 #   list of SML instructions
-
 #   define registers
-
 #   define memory and index
+class Simulator:
+    READ = 10
+    WRITE = 11
+    LOAD = 20
+    STORE = 21
+    ADD = 30
+    SUBTRACT = 31
+    DIVIDE = 32
+    MULTIPLY = 33
+    BRANCH = 40
+    BRANCHNEG = 41
+    BRANCHZERO = 42
+    HALT = 43
+
+    def __init__(self):
+        self.memory = []
+        for _ in range(100):
+            self.memory.append(None)
+        self.accumulator = None
+        self.index = 0
 
 # run_simulator() function - Jonah
 
