5d1d435c43b4f6f69896aa9ce78553fd6db57976
cinna96 <127256959+cinna96@users.noreply.github.com>
2025-03-22 16:43:24 -0600

    jonah segments done

--- main.py
+++ main.py
@@ -135,6 +135,10 @@ class UVSimGUI:
         if filename:
             self.file_entry.delete(0,tk.END)
             self.file_entry.insert(0,filename)
+            with open(filename, 'r') as file:
+                content = file.read()
+                self.code_editor.delete(1.0, tk.END)
+                self.code_editor.insert(tk.END, content)
 
     def run_program(self):
         self.output_text.delete(1.0,tk.END)
@@ -176,11 +180,6 @@ class UVSimGUI:
             for widget in self.root.winfo_children():
                 if isinstance(widget, tk.Button):
                     widget.config(bg=self.alt_color)
-
-if __name__ == "__main__":
-    root=tk.Tk()
-    gui=UVSimGUI(root)
-    root.mainloop()
 
 def validate():
     input_value = gui.input_entry.get()
@@ -277,3 +276,8 @@ class TestUVSim(unittest.TestCase):
         self.gui.file_entry.insert(0, "Test1.txt")
         filename = self.gui.file_entry.get()
         self.assertEqual(filename, "Test1.txt")
+
+if __name__ == "__main__":
+    root=tk.Tk()
+    gui=UVSimGUI(root)
+    root.mainloop()
